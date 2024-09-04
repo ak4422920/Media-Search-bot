06cf4a43081216adcea24f66772083b1e15c7c11
ak4422920 <ak4422920@gmail.com>
2024-09-04 10:39:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,8 +6,8 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
+API_ID = int(environ['29171167'])
+API_HASH = environ['7ea2149629e445936619f06a3c0dc716']
 BOT_TOKEN = environ['BOT_TOKEN']
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
